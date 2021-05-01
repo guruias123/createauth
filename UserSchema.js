fcfc1c494ebb4a10405e4e73bdd61dc7a0a5772f
guruias123 <74144270+guruias123@users.noreply.github.com>
2021-05-01 11:56:12 +0530

    Add files via upload

---
+++ UserSchema.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose');
+var UserSchema = new mongoose.Schema({
+    name:String,
+    companyname:String,
+    phonenumber:String,
+    companytype:String,
+    pincode:String,
+    email:String,
+    password:String,
+    role:String
+})
+
+mongoose.model('User',UserSchema);
+module.exports = mongoose.model('User');
